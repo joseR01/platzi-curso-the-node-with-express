6ecda6fc1cfb6b814453aefdc226dd234217c06c
Jose Rosales <josegregorio.5019@gmail.com>
2022-11-26 23:06:45 -0500

    agregando mas rutas get a la apis

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const express = require('express');
 const app = express();
 const PORT = 3000
@@ -5,6 +6,17 @@ const PORT = 3000
 app.get("/", (req, res) => {
   res.send("hola este es mi servidor web")
 })
+
+app.get("/ruta1", (req, res) => {
+  res.send("esta es tu nueva Ruta")
+});
+
+app.get("/ruta2", (req, res) => {
+  res.json({
+    name: "casa de mis suenos",
+    precio: 3000
+  })
+});
 
 app.listen(PORT, () => {
   console.log("puerto: " + PORT)
