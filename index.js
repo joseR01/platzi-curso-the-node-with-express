0cd38f8a91ee9ca5bcce04cf51cd7ed08ef4857c
Jose Rosales <josegregorio.5019@gmail.com>
2022-11-26 22:53:15 -0500

    instalando express y inicializando el servidor en el puerto 3000

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("esta es mi aplicacion")
+const express = require('express');
+const app = express();
+const PORT = 3000
+
+app.get("/", (req, res) => {
+  res.send("hola este es mi servidor web")
+})
+
+app.listen(PORT, () => {
+  console.log("puerto: " + PORT)
+});
