47d03e9171939861f1647f582c31a55cdfcf058b
Jose Rosales <josegregorio.5019@gmail.com>
2022-11-27 20:27:39 -0500

    creando las rutas put o path y deleted

--- routers/productos.js
+++ routers/productos.js
@@ -37,6 +37,24 @@ router.post("/", (req, res) => {
     message: "created",
     data: body
   })
-})
+});
 
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+
+  res.json({
+    message: "update",
+    date: { id, ...body },
+
+  })
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: "deleted",
+    id
+  })
+});
 module.exports = router;
