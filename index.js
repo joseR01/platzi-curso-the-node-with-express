cb53e9503acf38165897a339932a521c457ae3f2
Jose Rosales <josegregorio.5019@gmail.com>
2022-12-01 15:19:45 -0400

    agregar libreria y configuraciones para resolver problemas de cors

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 /* eslint-disable no-console */
 const express = require('express');
-const RouterApi = require("./routers")
-const { logErrors, errorHandler, boomErrorHandler } = require("./middlewares/error")
+const cors = require('cors');
+const RouterApi = require('./routers');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error');
 
 const app = express();
-const PORT = 3000
+const PORT = 3000;
 
-app.use(express.json())
-app.get("/", (req, res) => {
-  res.send("hola este es mi servidor web")
-})
+app.use(express.json());
+
+const whitelist = ['http://localhost:3000', 'http://localhost:8080'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  },
+};
+app.use(cors(options));
+
+app.get('/', (req, res) => {
+  res.send('hola este es mi servidor web');
+});
 
 RouterApi(app);
 app.use(logErrors);
@@ -17,5 +35,5 @@ app.use(boomErrorHandler);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log("puerto: " + PORT)
+  console.log('puerto: ' + PORT);
 });
