e009ef64e619e7ae4119ef52de8b2bef1c2f61a0
Jose Rosales <josegregorio.5019@gmail.com>
2022-12-01 15:00:17 -0400

    Probando nuestro EndPoint

--- routers/productos.js
+++ routers/productos.js
@@ -1,75 +1,93 @@
-const express = require("express")
-const ProductsService = require("../service/productos");
+const express = require('express');
+const ProductsService = require('../service/productos');
+const { validatorHandler } = require('../middlewares/valitate');
+const {
+  getProductScheman,
+  createProductScheman,
+  updateProductScheman,
+} = require('../schemas/product');
 
+const router = express.Router();
+const service = new ProductsService();
 
-const router = express.Router()
-const service = new ProductsService()
-
-router.get("/", async (req, res) => {
-  const productos = await service.find()
-  res.json({ size: productos.length, data: productos })
+router.get('/', async (req, res) => {
+  const productos = await service.find();
+  res.json({ size: productos.length, data: productos });
 });
 
-router.get("/filter", (req, res) => {
-  res.send("aqui vamos a filtrar");
-})
+router.get('/filter', (req, res) => {
+  res.send('aqui vamos a filtrar');
+});
 
-router.get("/:id", async (req, res, next) => {
-
-  try {
-    const { id } = req.params;
-    const products = await service.findOne(id)
-    if (products) {
-      res.status(200).json(products);
-    } else {
-      res.status(404).json({
-        message: "no se encontro el producto"
-      });
-
+router.get(
+  '/:id',
+  validatorHandler(getProductScheman, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const products = await service.findOne(id);
+      if (products) {
+        res.status(200).json(products);
+      } else {
+        res.status(404).json({
+          message: 'no se encontro el producto',
+        });
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error)
   }
-
-})
-router.post("/", async (req, res) => {
-  const body = req.body;
-  const newProducto = await service.create(body)
-
-  res.status(201).json({
-    message: "created",
-    data: newProducto
-  })
-});
-
-router.patch("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
+);
+router.post(
+  '/',
+  validatorHandler(createProductScheman, 'body'),
+  async (req, res) => {
     const body = req.body;
-    const product = await service.update(id, body);
-    res.json({
-      message: "update",
-      date: { product },
+    const newProducto = await service.create(body);
 
-    })
-  } catch (error) {
-    next(error)
+    res.status(201).json({
+      message: 'created',
+      data: newProducto,
+    });
   }
-});
+);
 
-router.delete("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const products = await service.delete(id)
-
-    res.json({
-      message: "deleted",
-      products
-    })
-  } catch (error) {
-    res.status(404).json({
-      message: error.message
-    })
+router.patch(
+  '/:id',
+  validatorHandler(getProductScheman, 'params'),
+  validatorHandler(updateProductScheman, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const product = await service.update(id, body);
+      res.json({
+        message: 'update',
+        date: { product },
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
+
+router.delete(
+  '/:id',
+  validatorHandler(getProductScheman, 'params'),
+  async (req, res) => {
+    try {
+      const { id } = req.params;
+      const products = await service.delete(id);
+
+      res.json({
+        message: 'deleted',
+        products,
+      });
+    } catch (error) {
+      res.status(404).json({
+        message: error.message,
+      });
+    }
+  }
+);
 module.exports = router;
--- schemas/product.js
+++ schemas/product.js
@@ -1,17 +1,20 @@
 const join = require('joi');
 
 const id = join.string().uuid();
-const name = join.string().alphanum().min(3).max(15);
+const name = join.string().min(3).max(15);
 const price = join.number().integer().min(10);
+const image = join.string().uri();
 
 const createProductScheman = join.object({
   name: name.required(),
   price: price.required(),
+  image: image.required(),
 });
 
 const updateProductScheman = join.object({
   name: name,
   price: price,
+  image: image,
 });
 
 const getProductScheman = join.object({
